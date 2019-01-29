ea62a2eb73bddf75c9e1841beb7ccb69dc0ddf38
Jonathan Romero <vyr0n@live.com.mx>
2019-01-28 23:45:49 -0600

    functions and style files with data.

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Miradas Divergentes Wordpress Theme functions and definitions
+ * 
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ * 
+ * @package WordPress
+ * @subpackage mdwt
+ * @since 1.0.0
+ * @version 1.0.0
+ */
--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html <?php language_attributes();?>>
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Miradas Divergentes</title>
+    <title><?php wp_title('|',true,'right'); ?></title>
 </head>
 <body>
     <header class="Header">
